e0212641a3d58118367ea7db9a5868a0c05c88f8
Rhuan Carvalho <rsjlcarvalho@gmail.com>
2025-03-27 19:16:55 -0300

    feat: call API to create new schedule

--- Vollmed/Services/WebService.swift
+++ Vollmed/Services/WebService.swift
@@ -7,11 +7,36 @@
 
 import UIKit
 
+let patientID: String = "b3e789f1-5287-4fc0-a3ac-c62c6a612e1f"
+
 struct WebService {
     
     private let baseURL = "http://localhost:3000"
-    
     private let imageCache = NSCache<NSString, UIImage>()
+    
+    func scheduleAppointment(specialistID: String, patientID: String, date: String) async throws -> ScheduleAppointmentResponse? {
+        let endpoint = "\(baseURL)/consulta"
+        
+        guard let url = URL(string: endpoint) else {
+            print("Erro na URL!")
+            return nil
+        }
+        
+        let appointment = ScheduleAppointmentRequest(specialist: specialistID, patient: patientID, date: date)
+        
+        let jsonData = try JSONEncoder().encode(appointment)
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpBody = jsonData
+        
+        let (data, _) = try await URLSession.shared.data(for: request)
+        let appointmentResponse = try JSONDecoder().decode(ScheduleAppointmentResponse.self, from: data)
+        
+        return appointmentResponse
+        
+    }
     
     func downloadImage(from imageURL: String) async throws -> UIImage? {
         guard let url = URL(string: imageURL) else {

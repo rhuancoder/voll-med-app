f64ae95c6cd860e5a20684c2faaeb4abc8f8eb59
Rhuan Carvalho <rsjlcarvalho@gmail.com>
2025-03-31 20:23:35 -0300

    feat: add my appointments view

--- Vollmed/ContentView.swift
+++ Vollmed/ContentView.swift
@@ -9,13 +9,25 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView {
+            NavigationStack {
+                HomeView()
+            }
+            .tabItem {
+                Label(
+                    title: { Text("Home") },
+                    icon: { Image(systemName: "house")})
+            }
+            
+            NavigationStack {
+                MyAppointmentsView()
+            }
+            .tabItem {
+                Label(
+                    title: { Text("Minhas consultas") },
+                    icon: { Image(systemName: "calendar")})
+            }
         }
-        .padding()
     }
 }
 

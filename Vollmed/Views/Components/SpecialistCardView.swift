19f79d490bf5e9c5be54368374a44bf4f6d495bc
Rhuan Carvalho <rsjlcarvalho@gmail.com>
2025-04-02 20:53:16 -0300

    feat: add reschedule view

--- Vollmed/Views/Components/SpecialistCardView.swift
+++ Vollmed/Views/Components/SpecialistCardView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct SpecialistCardView: View {
     
     var specialist: Specialist
+    var appointment: Appointment?
     
     let service = WebService()
     
@@ -42,13 +43,35 @@ struct SpecialistCardView: View {
                         .font(.title3)
                         .bold()
                     Text(specialist.specialty)
+                    
+                    if let appointment {
+                        Text(appointment.date.convertDateStringToReadableDate())
+                            .bold()
+                    }
                 }
             }
             
-            NavigationLink {
-                ScheduleAppointmentView(specialistID: specialist.id)
-            } label: {
-                ButtonView(text: "Agendar consulta")
+            if let appointment {
+                HStack {
+                    
+                    NavigationLink {
+                        ScheduleAppointmentView(specialistID: appointment.specialist.id, isRescheduleView: true, appointmentID: appointment.id)
+                    } label: {
+                        ButtonView(text: "Remarcar")
+                    }
+                    
+                    Button(action: {
+                        print("Botão de cancelar pressionado!")
+                    }, label: {
+                        ButtonView(text: "Cancelar", buttonType: .cancel)
+                    })
+                }
+            } else {
+                NavigationLink {
+                    ScheduleAppointmentView(specialistID: specialist.id)
+                } label: {
+                    ButtonView(text: "Agendar consulta")
+                }
             }
         }
         .frame(maxWidth: .infinity, alignment: .leading)
@@ -65,12 +88,12 @@ struct SpecialistCardView: View {
 
 #Preview {
     SpecialistCardView(specialist:
-        Specialist(id: "c84k5kf",
-                   name: "Dr. Carlos Alberto",
-                   crm: "123456",
-                   imageUrl: "https://images.unsplash.com/photo-1637059824899-a441006a6875?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=752&q=80",
-                   specialty: "Neurologia",
-                   email: "carlos.alberto@example.com",
-                   phoneNumber: "(11) 99999-9999"
-                  ))
+                        Specialist(id: "c84k5kf",
+                                   name: "Dr. Carlos Alberto",
+                                   crm: "123456",
+                                   imageUrl: "https://images.unsplash.com/photo-1637059824899-a441006a6875?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=752&q=80",
+                                   specialty: "Neurologia",
+                                   email: "carlos.alberto@example.com",
+                                   phoneNumber: "(11) 99999-9999"
+                                  ))
 }

d930b612b1bf3e0ef5d5f7edb622b8626371056d
Rhuan Carvalho <rhuancoder@gmail.com>
2026-05-17 12:44:57 -0300

    refactor: implements networking services

---
+++ Vollmed/Networking/Endpoints/AuthenticationEndpoint.swift
@@ -0,0 +1,47 @@
+//
+//  AuthenticationEndpoint.swift
+//  Vollmed
+//
+//  Created by Rhuan Carvalho on 17/05/26.
+//
+
+import Foundation
+
+enum AuthenticationEndpoint {
+    case logout
+}
+
+extension AuthenticationEndpoint: Endpoint {
+    var path: String {
+        switch self {
+        case .logout:
+            return "/auth/logout"
+        }
+    }
+    
+    var method: RequestMethod {
+        switch self {
+        case .logout:
+            return .post
+        }
+    }
+    
+    var header: [String : String]? {
+        switch self {
+        case .logout:
+            guard let token = AuthenticationManager.shared.token else { return nil }
+            
+            return [
+                "Authorization": "Bearer \(token)",
+                "Content-Type": "application/json"
+            ]
+        }
+    }
+    
+    var body: [String : String]? {
+        switch self {
+        case .logout:
+            return nil
+        }
+    }
+}

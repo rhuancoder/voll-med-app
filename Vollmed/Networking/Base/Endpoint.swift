563447c2f58cf7789fa6ed5ba4434581a56b4b8c
Rhuan Carvalho <rhuancoder@gmail.com>
2026-05-17 11:53:14 -0300

    feat: implement http client protocol

---
+++ Vollmed/Networking/Base/Endpoint.swift
@@ -0,0 +1,22 @@
+//
+//  Endpoint.swift
+//  Vollmed
+//
+//  Created by Rhuan Carvalho on 17/05/26.
+//
+
+import Foundation
+
+protocol Endpoint {
+    var scheme: String { get }
+    var host: String { get }
+    var path: String { get }
+    var method: RequestMethod { get }
+    var header: [String: String] { get }
+    var body: [String: String]? { get }
+}
+
+extension Endpoint {
+    var scheme: String { return "http" }
+    var host: String { return "localhost" }
+}
